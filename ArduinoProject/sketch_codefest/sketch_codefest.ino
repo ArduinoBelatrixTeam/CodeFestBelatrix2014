75d4e137a4ae5160afc78a85c85636fe8a4a5fb6
Luis Silva <lesilvav@gmail.com>
2014-12-15 23:03:20 -0500

    Unnecessary code was removed.

--- ArduinoProject/sketch_codefest/sketch_codefest.ino
+++ ArduinoProject/sketch_codefest/sketch_codefest.ino
@@ -1,69 +1,43 @@
-/*
- *  Author: CodeFest Arduino Team
- *  Date  : November 2014
- *  
- *  This program is free software: you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation, either version 3 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 #include <SoftwareSerial.h>
 #include <NewPing.h>
 
-#define RxD 10
-#define TxD 11
-#define RST 5
-#define KEY 4
-
+//Proximity Sensor definition
 #define TRIGGER_PIN  2  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 200 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
+unsigned int uS; // Ping time in microseconds
+int distance; //distance in cm or m
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); // NewPing setup of pins and maximum distance.
-SoftwareSerial BTSerial(RxD, TxD);
-unsigned int uS; // Microseconds
 
-void setup()
-{
-  
-  pinMode(RST, OUTPUT);
-  pinMode(KEY, OUTPUT);
-  digitalWrite(RST, LOW);
-  digitalWrite(KEY, HIGH);
-  //digitalWrite(KEY, LOW);
-  digitalWrite(RST, HIGH);
-  
-  delay(500);
-  
-  BTSerial.flush();
-  delay(500);
-  BTSerial.begin(9600);
+//BlueTooth Definition
+#define ARD_RX 10 //Arduino pin tied to BlueTooth serial TX
+#define ARD_TX 11 //Arduino pin tied to BlueTooth serial RX
+
+SoftwareSerial btSerial(ARD_RX, ARD_TX);
+
+void setup() {
   Serial.begin(9600);
-  
-  delay(100);
+  Serial.println("Enter AT command:");
+  btSerial.begin(9600);
+  btSerial.flush();
 }
 
-void loop()
-{
+void loop(){
   // Send ping, get ping time in microseconds (uS).
   uS = sonar.ping(); 
   
   // Convert ping time to distance in cm and print result (0 = outside set distance range)
-  int distance_calculated = (uS / US_ROUNDTRIP_CM);
+  int distance = (uS / US_ROUNDTRIP_CM);
   
-  BTSerial.print(distance_calculated);
-  BTSerial.print("#");
-  Serial.println(distance_calculated);
+  //write distance in Serial
+  Serial.println(distance);
   
-  delay(50);
-}
+  //write distance in BlueTooth
+  btSerial.print(distance);
+  btSerial.print("#");
+  
+  //wait before sending the next ping
+  delay(100);
 
+}
