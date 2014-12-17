c5a3b7b0efb3317d0b25c8167f22da15d410bf2f
Luis Silva <lesilvav@gmail.com>
2014-12-16 23:02:35 -0500

    Unnecessary code was removed.

--- RearSensorDroid/app/src/main/java/com/codefest/rearsensordroid/MyActivity.java
+++ RearSensorDroid/app/src/main/java/com/codefest/rearsensordroid/MyActivity.java
@@ -4,22 +4,16 @@ import android.app.ListActivity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
-import android.content.Context;
 import android.content.Intent;
-import android.media.AudioFormat;
-import android.media.AudioManager;
-import android.media.AudioTrack;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.os.PowerManager;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -34,50 +28,24 @@ import java.util.UUID;
 
 public class MyActivity extends ListActivity {
 
-
-    private final int duration = 1; // seconds
-    private final int sampleRate = 8000;
-    private final int numSamples = duration * sampleRate;
-    private final double sample[] = new double[numSamples];
-    private final double freqOfTone = 500; // hz
-
-    private final byte generatedSnd[] = new byte[2 * numSamples];
-
-//    private Button btToggle;
     private ArrayAdapter<String> mArrayAdapter;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothSocket btSocket;
     private ArrayList<BluetoothDevice> btDeviceArray = new ArrayList<BluetoothDevice>();
     private ConnectAsyncTask connectAsyncTask;
-    private ConnectedThread contecThread;
-    TextView distancia;
+    private ConnectedThread connectedThread;
+    TextView distance;
 
-    private String buffer = "";
-
-    private Handler soundHandler = new Handler();
-    Thread thread_;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //application keeps the screen on
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_main);
 
-        // Use a new tread as this can take a while
-        thread_ = new Thread(new Runnable() {
-            public void run() {
-                genTone();
-                soundHandler.post(new Runnable() {
-
-                    public void run() {
-                        playSound();
-                    }
-                });
-            }
-        });
-
-
-
-        distancia = (TextView) findViewById(R.id.distancia);
-        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        distance = (TextView) findViewById(R.id.distancia);
 
         mArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
         setListAdapter(mArrayAdapter);
@@ -85,7 +53,7 @@ public class MyActivity extends ListActivity {
         // Instance AsyncTask
         connectAsyncTask = new ConnectAsyncTask();
 
-        //Get Bluettoth Adapter
+        //Get Bluetooth Adapter
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         // Check smartphone support Bluetooth
@@ -101,27 +69,21 @@ public class MyActivity extends ListActivity {
             startActivityForResult(enableBtIntent, 1);
         }
 
-        // Queryng paried devices
-        Set<BluetoothDevice> pariedDevices = mBluetoothAdapter.getBondedDevices();
-        if (pariedDevices.size() > 0) {
-            for (BluetoothDevice device : pariedDevices) {
+        // Querying paired devices
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {
+            for (BluetoothDevice device : pairedDevices) {
                 mArrayAdapter.add(device.getName() + "\n" + device.getAddress());
                 btDeviceArray.add(device);
             }
         }
 
-//        btToggle = (Button) findViewById(R.id.btToggle);
-//        btToggle.setOnClickListener(btToggleOnClickListener);
-
     }
-
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-
         BluetoothDevice device = btDeviceArray.get(position);
         connectAsyncTask.execute(device);
-
     }
 
     // Click event on Button
@@ -129,11 +91,9 @@ public class MyActivity extends ListActivity {
 
         @Override
         public void onClick(View v) {
-            contecThread.write(new String("L").getBytes());
-
+            connectedThread.write(new String("L").getBytes());
         }
     };
-
 
     private class ConnectAsyncTask extends AsyncTask<BluetoothDevice, Integer, BluetoothSocket> {
 
@@ -146,11 +106,9 @@ public class MyActivity extends ListActivity {
             mmDevice = device[0];
 
             try {
-
                 String mmUUID = "00001101-0000-1000-8000-00805F9B34FB";
                 mmSocket = mmDevice.createInsecureRfcommSocketToServiceRecord(UUID.fromString(mmUUID));
                 mmSocket.connect();
-
             } catch (Exception e) {
             }
 
@@ -159,18 +117,12 @@ public class MyActivity extends ListActivity {
 
         @Override
         protected void onPostExecute(BluetoothSocket result) {
-
             btSocket = result;
-            //Enable Button
-//            btToggle.setEnabled(true);
-            contecThread = new ConnectedThread(btSocket);
-            contecThread.start();
-
+            connectedThread = new ConnectedThread(btSocket);
+            connectedThread.start();
         }
 
-
     }
-
 
     /**
      * Created by Carlos on 11/15/2014.
@@ -202,7 +154,6 @@ public class MyActivity extends ListActivity {
                     bytes += mmInStream.read(buffer, bytes, buffer.length - bytes);
                     for (int i = begin; i < bytes; i++) {
                         if (buffer[i] == "#".getBytes()[0]) {
-//                            thread.start();
                             mHandler.obtainMessage(1, begin, i, buffer).sendToTarget();
                             begin = i + 1;
                             if (i == bytes - 1) {
@@ -234,8 +185,6 @@ public class MyActivity extends ListActivity {
     }
 
 
-
-
     Handler mHandler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
@@ -247,52 +196,22 @@ public class MyActivity extends ListActivity {
                 case 1:
                     String writeMessage = new String(writeBuf);
                     writeMessage = writeMessage.substring(begin, end);
-//                    buffer = buffer +writeMessage;
                     updateText(writeMessage);
                     break;
             }
         }
     };
 
-    void genTone(){
-        // fill out the array
-        for (int i = 0; i < numSamples; ++i) {
-            sample[i] = Math.sin(2 * Math.PI * i / (sampleRate/freqOfTone));
-        }
-
-        // convert to 16 bit pcm sound array
-        // assumes the sample buffer is normalised.
-        int idx = 0;
-        for (final double dVal : sample) {
-            // scale to maximum amplitude
-            final short val = (short) ((dVal * 32767));
-            // in 16 bit wav PCM, first byte is the low order byte
-            generatedSnd[idx++] = (byte) (val & 0x00ff);
-            generatedSnd[idx++] = (byte) ((val & 0xff00) >>> 8);
-
-        }
-    }
-
-    void playSound(){
-        final AudioTrack audioTrack = new AudioTrack(AudioManager.STREAM_MUSIC,
-                sampleRate, AudioFormat.CHANNEL_OUT_MONO,
-                AudioFormat.ENCODING_PCM_16BIT, generatedSnd.length,
-                AudioTrack.MODE_STATIC);
-        audioTrack.write(generatedSnd, 0, generatedSnd.length);
-        audioTrack.play();
-    }
-
     void updateText(String text){
         text = text.replace("#","").trim();
         if(text!="") {
             int value = Integer.valueOf(text);
             if (value != 0) {
-                distancia.setText(text + " cm");
+                distance.setText(text + " cm");
                 if (value < 40) {
-
-                    distancia.setTextSize(250);
+                    distance.setTextSize(250);
                 } else {
-                    distancia.setTextSize(180);
+                    distance.setTextSize(180);
                 }
             }
         }
