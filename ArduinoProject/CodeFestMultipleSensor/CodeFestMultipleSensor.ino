f01ddb9ec04f6f11b65a2c9f11a7cf218a443f60
Luis Silva <lesilvav@gmail.com>
2015-03-24 21:28:57 -0500

    Arduino sketch supporting multiple sensors. Actually, ready for two now.

---
+++ ArduinoProject/CodeFestMultipleSensor/CodeFestMultipleSensor.ino
@@ -0,0 +1,81 @@
+/*
+ *  Author: CodeFest Arduino Team
+ *  Date  : November 2014
+ *  Modified: December 14 by Luis Silva
+ *            March 14 by Luis Silva - Multiple Sensor Logic
+ *  
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+#include <SoftwareSerial.h>
+#include <NewPing.h>
+
+#define SONAR_NUM     2 // Number or sensors.
+#define MAX_DISTANCE 200 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
+
+unsigned long pingTimer[SONAR_NUM]; // Holds the times when the next ping should happen for each sensor.
+unsigned int cm[SONAR_NUM];         // Where the ping distances are stored.
+uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
+String sensorValues;
+
+NewPing sonar[SONAR_NUM] = {     // Sensor object array.
+  NewPing(2, 3, MAX_DISTANCE), // Each sensor's trigger pin, echo pin, and max distance to ping.
+  NewPing(4, 5, MAX_DISTANCE)
+};
+
+//BlueTooth Definition
+#define ARD_RX 10 //Arduino pin tied to BlueTooth serial TX
+#define ARD_TX 11 //Arduino pin tied to BlueTooth serial RX
+
+SoftwareSerial btSerial(ARD_RX, ARD_TX);
+
+void setup() {
+  Serial.begin(9600);
+  btSerial.begin(9600);
+  btSerial.flush();  
+  pingTimer[0] = millis() + 75;           // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  for (uint8_t i = 1; i < SONAR_NUM; i++) // Set the starting time for each sensor.
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+}
+
+void loop() {
+  for (uint8_t i = 0; i < SONAR_NUM; i++) { // Loop through all the sensors.
+    if (millis() >= pingTimer[i]) {         // Is it this sensor's time to ping?
+      pingTimer[i] += PING_INTERVAL * SONAR_NUM;  // Set next time this sensor will be pinged.
+      if (i == 0 && currentSensor == SONAR_NUM - 1) oneSensorCycle(); // Sensor ping cycle complete, do something with the results.
+      sonar[currentSensor].timer_stop();          // Make sure previous timer is canceled before starting a new ping (insurance).
+      currentSensor = i;                          // Sensor being accessed.
+      cm[currentSensor] = 0;                      // Make distance zero in case there's no ping echo for this sensor.
+      sonar[currentSensor].ping_timer(echoCheck); // Do the ping (processing continues, interrupt will call echoCheck to look for echo).
+    }
+  }
+  // The rest of your code would go here.
+}
+
+void echoCheck() { // If ping received, set the sensor distance to array.
+  if (sonar[currentSensor].check_timer())
+    cm[currentSensor] = sonar[currentSensor].ping_result / US_ROUNDTRIP_CM;
+}
+
+void oneSensorCycle() { // Sensor ping cycle complete, do something with the results.
+  for (uint8_t i = 0; i < SONAR_NUM; i++) {
+    sensorValues = i + String("=") + cm[i] + String(",");
+    Serial.print(sensorValues);    
+    btSerial.print(cm[i]);
+    btSerial.print(String(","));
+  }
+  btSerial.print("#");
+  Serial.println(); 
+}
